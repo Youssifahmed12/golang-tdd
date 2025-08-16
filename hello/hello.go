584965692737ce54f86d31a756136982354b02f0
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-16 17:16:20 +0300

    refactoring

--- hello/hello.go
+++ hello/hello.go
@@ -1,15 +1,32 @@
 package main
 
+import "strings"
+
+const (
+	englishprfx = "Hello "
+	spanishprfx = "Hola "
+	frenchPrfx  = "Bonjour "
+)
+
 func main() {
 
 }
 
 func hello(name, lang string) string {
-	if lang == "Spanish" {
-		return "Hola " + name + "!"
+	return getLangPrefix(lang) + name + "!"
+}
+
+func getLangPrefix(lang string) string {
+	lang = strings.ToLower(lang)
+	switch lang {
+	case "english":
+		return englishprfx
+	case "spanish":
+		return spanishprfx
+	case "french":
+		return frenchPrfx
+	default:
+		return englishprfx
 	}
-	if lang == "French" {
-		return "Bonjour " + name + "!"
-	}
-	return "Hello " + name + "!"
+
 }
