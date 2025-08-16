de552a5fa290d326ce785fa66af300d0002e7d14
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-16 17:04:49 +0300

    adding support for diffrent languages

--- hello/hello.go
+++ hello/hello.go
@@ -4,6 +4,12 @@ func main() {
 
 }
 
-func hello(name string) string {
+func hello(name, lang string) string {
+	if lang == "Spanish" {
+		return "Hola " + name + "!"
+	}
+	if lang == "French" {
+		return "Bonjour " + name + "!"
+	}
 	return "Hello " + name + "!"
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello Youssif!"
-	got := hello("Youssif")
-	if want != got {
-		t.Errorf("wanted %q but got %q", want, got)
-	}
+	t.Run("English Hello Test with name", func(t *testing.T) {
+		want := "Hello Youssif!"
+		got := hello("Youssif", "English")
+		if want != got {
+			t.Errorf("wanted %q but got %q", want, got)
+		}
+	})
+	t.Run("Spanish Hello Test with name", func(t *testing.T) {
+		want := "Hola Youssif!"
+		got := hello("Youssif", "Spanish")
+		if want != got {
+			t.Errorf("wanted %q but got %q", want, got)
+		}
+	})
+	t.Run("French Hello Test with name", func(t *testing.T) {
+		want := "Bonjour Youssif!"
+		got := hello("Youssif", "French")
+		if want != got {
+			t.Errorf("wanted %q but got %q", want, got)
+		}
+	})
 }
