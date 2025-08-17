80212b5c87822be382394cc1e44d3a571f4f8183
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-17 08:59:28 +0300

    add wallet deposit , balance

---
+++ pointers/wallet_test.go
@@ -0,0 +1,13 @@
+package wallet
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(Bitcoin(10))
+	got := wallet.Balance()
+	want := Bitcoin(10)
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
