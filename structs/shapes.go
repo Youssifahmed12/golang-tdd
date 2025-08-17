dd8f75083f386adbfab7f435a621e75fc163d326
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-17 07:33:13 +0300

    adding circle struct and refactoring to use reciever methods

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,8 +1,22 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	height float64
 	width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.height * r.width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(r Rectangle) float64 {
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,10 +15,19 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("Calculate the Area of a rectangle", func(t *testing.T) {
 		r := Rectangle{10.0, 10.0}
-		got := Area(r)
+		got := r.Area()
 		want := 100.0
 		if got != want {
 			t.Errorf("got %.2f wanted %.2f", got, want)
 		}
 	})
+	t.Run("Calculate the Area of a circle", func(t *testing.T) {
+		c := Circle{10}
+		got := c.Area()
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %g wanted %g", got, want)
+		}
+	})
+
 }
