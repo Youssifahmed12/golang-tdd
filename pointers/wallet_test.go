060698e4858f8205848823ea2bcfee032ce402f3
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-17 23:40:49 +0300

    withdraw insuffiecent funds

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -11,6 +11,15 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
+	assertError := func(t testing.TB, got error, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("should've got an error")
+		}
+		if got != want {
+			t.Errorf("got %q want %q ", got, want)
+		}
+	}
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -21,6 +30,15 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficent funds", func(t *testing.T) {
+		startingBalance := Bitcoin(10)
+		wallet := Wallet{balance: startingBalance}
+		got := wallet.Withdraw(Bitcoin(20))
+		assertError(t, got, ErrInsufficentFunds)
+		assertBalance(t, wallet, startingBalance)
+
 	})
 
 }
