15a486a1b78499304a2d7a63befc138d2e619862
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-18 02:07:19 +0300

    refactoring

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -6,11 +6,15 @@ type Dictionary map[string]string
 
 var ErrWordDoesntExist = errors.New("word doesn't exist")
 
-func (d Dictionary) Search(dic map[string]string, key string) (string, error) {
+func (d Dictionary) Search(key string) (string, error) {
 	// ok will be a boolean , true if key is found false if isn't
-	definition, ok := dic[key]
+	definition, ok := d[key]
 	if !ok {
 		return "", ErrWordDoesntExist
 	}
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, wordDefintion string) {
+	d[word] = wordDefintion
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -2,12 +2,12 @@ package maps
 
 import "testing"
 
-func TestMap(t *testing.T) {
+func TestSearch(t *testing.T) {
 	t.Run("find string available in dictionary", func(t *testing.T) {
 		dic := Dictionary{"test": "this is a test"}
 
 		want := "this is a test"
-		got, _ := dic.Search(dic, "test")
+		got, _ := dic.Search("test")
 
 		assertStrings(t, got, want)
 	})
@@ -15,10 +15,33 @@ func TestMap(t *testing.T) {
 		dic := Dictionary{"nottest": "this is a test"}
 
 		want := ErrWordDoesntExist
-		_, got := dic.Search(dic, "test")
+		_, got := dic.Search("test")
 		assertError(t, got, want)
 
 	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("adding to the dictionary", func(t *testing.T) {
+		var (
+			word          = "word"
+			wordDefintion = "word definition"
+		)
+
+		dic := Dictionary{}
+		dic.Add(word, wordDefintion)
+		assertDefinition(t, dic, word, wordDefintion)
+	})
+}
+
+func assertDefinition(t testing.TB, d Dictionary, word string, wordDef string) {
+	t.Helper()
+	got, err := d.Search(word)
+
+	if err != nil {
+		t.Fatal("should've found word")
+	}
+	assertStrings(t, got, wordDef)
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -29,6 +52,7 @@ func assertStrings(t testing.TB, got, want string) {
 }
 
 func assertError(t testing.TB, got, want error) {
+	t.Helper()
 	if got == nil {
 		t.Fatal("wanted an error but didn't get one")
 	}
