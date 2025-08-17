d63746cee488d1c6d7c3f5248b69f9670f1c4403
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-18 00:53:36 +0300

    handle non existent words

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,16 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(dic map[string]string, key string) string {
-	return dic[key]
+var ErrWordDoesntExist = errors.New("word doesn't exist")
+
+func (d Dictionary) Search(dic map[string]string, key string) (string, error) {
+	// ok will be a boolean , true if key is found false if isn't
+	definition, ok := dic[key]
+	if !ok {
+		return "", ErrWordDoesntExist
+	}
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,15 +3,36 @@ package maps
 import "testing"
 
 func TestMap(t *testing.T) {
-	dic := Dictionary{"test": "this is a test"}
-	want := "this is a test"
-	got := dic.Search(dic, "test")
-	assertStrings(t, got, want)
+	t.Run("find string available in dictionary", func(t *testing.T) {
+		dic := Dictionary{"test": "this is a test"}
+
+		want := "this is a test"
+		got, _ := dic.Search(dic, "test")
+
+		assertStrings(t, got, want)
+	})
+	t.Run("find non existent string in dictionary", func(t *testing.T) {
+		dic := Dictionary{"nottest": "this is a test"}
+
+		want := ErrWordDoesntExist
+		_, got := dic.Search(dic, "test")
+		assertError(t, got, want)
+
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q given", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
