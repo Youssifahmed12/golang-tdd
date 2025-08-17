48575e668747ba935c36c03acd5ab5ab430c0857
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-18 00:05:19 +0300

    refactoring assert

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,6 +1,5 @@
 package maps
 
-func Search(dic map[string]string, key string) (value string) {
-	value = dic[key]
-	return
+func Search(dic map[string]string, key string) string {
+	return dic[key]
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -6,8 +6,12 @@ func TestMap(t *testing.T) {
 	dic := map[string]string{"test": "this is a test"}
 	want := "this is a test"
 	got := Search(dic, "test")
+	assertStrings(t, got, want)
+}
 
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, "test")
+		t.Errorf("got %q want %q given", got, want)
 	}
 }
