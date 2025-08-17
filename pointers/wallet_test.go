0a7afbc76c66b28af5acb7a443aaa48eb1d907ac
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-17 23:48:37 +0300

    add assertNoError thanks to the observation of the linter

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -11,7 +11,8 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -32,6 +33,13 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal("welp , there's an error which shouldnt be there")
 	}
 }
 func assertError(t testing.TB, got error, want error) {
