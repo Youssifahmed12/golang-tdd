72d574e978532e28fc6a1365912ff739310cc80b
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-27 07:27:04 +0300

    learning about reflection

---
+++ reflection/reflection.go
@@ -0,0 +1,16 @@
+package reflection
+
+import (
+	"reflect"
+)
+
+func walk(x interface{}, fn func(input string)) {
+	value := reflect.ValueOf(x)
+	for i := 0; i < value.NumField(); i++ {
+		field := value.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+	}
+}
