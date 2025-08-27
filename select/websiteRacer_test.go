2f8262fdcfb91f72c75404de7c64e90b2635cef4
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-27 04:32:00 +0300

    implementing the timeout feature

--- select/websiteRacer_test.go
+++ select/websiteRacer_test.go
@@ -9,21 +9,40 @@ import (
 
 func TestRacer(t *testing.T) {
 
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("returns the URL of the faster server", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if err != nil {
+			t.Fatalf("got an error when none is expected")
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("return err if timeout is 10 seconds", func(t *testing.T) {
+		server := makeDelayedServer(20 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, time.Millisecond)
+
+		if err == nil {
+			t.Fatal("Should be an error")
+		}
+
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
