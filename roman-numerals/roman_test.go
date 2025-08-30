678c775960121e40beef72eabb84c828ebda30d8
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-30 07:04:28 +0300

    cleaned up the assertion func

--- roman-numerals/roman_test.go
+++ roman-numerals/roman_test.go
@@ -2,7 +2,6 @@ package romannumerals
 
 import (
 	"fmt"
-	"log"
 	"testing"
 	"testing/quick"
 )
@@ -67,15 +66,14 @@ func TestConvertingToArabic(t *testing.T) {
 
 func TestPropertiesOfConversion(t *testing.T) {
 	assertion := func(arabic uint16) bool {
-		if arabic < 0 || arabic > 3999 {
-			log.Println(arabic)
+		if arabic > 3999 {
 			return true
 		}
+		t.Log("testing", arabic)
 		roman := ConvertToRoman(arabic)
 		fromRoman := ConvertToArabic(roman)
 		return fromRoman == arabic
 	}
-
 	if err := quick.Check(assertion, nil); err != nil {
 		t.Error("failed checks", err)
 	}
