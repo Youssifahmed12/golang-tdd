6a3bc1c6a4cd508a1c73018ecb24029a7bfbbc43
Youssifahmed12 <youssefbro12397@gmail.com>
2025-09-01 22:11:54 +0300

    getting posts title from folder

--- reading-files/blogposts/blogpost.go
+++ reading-files/blogposts/blogpost.go
@@ -1,20 +1,50 @@
 package blogposts
 
-import "io/fs"
+import (
+	"io"
+	"io/fs"
+)
 
 type Post struct {
-	Title string
+	Title       string
+	Description string
 }
 
-func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
-	dir, err := fs.ReadDir(fileSystem, ".")
+func NewPostsFromFS(fsys fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fsys, ".")
 
 	if err != nil {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fsys, f.Name())
+		if err != nil {
+			continue
+		}
+
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fsys fs.FS, fname string) (Post, error) {
+	postFile, err := fsys.Open(fname)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	return newPost(postFile)
+
+}
+
+func newPost(postFile io.Reader) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
